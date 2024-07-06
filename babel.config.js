622de64eace512cbfd2ae1c71226f178b2276581
Roberto <rmancinetti@hotmail.it>
2024-07-06 11:15:19 +0200

    commit iniziale /2

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
 }
-=======
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
->>>>>>> a0929fdf8fb41b402091ae03dd9287e71c7aaa79
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
@@ -9,15 +8,3 @@ const config: CapacitorConfig = {
 };
 
 export default config;
-=======
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'terminal.ionic.starter',
-  appName: 'terminal',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
-
-export default config;
->>>>>>> a0929fdf8fb41b402091ae03dd9287e71c7aaa79
